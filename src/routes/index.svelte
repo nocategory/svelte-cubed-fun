9b411a2f295d8d8bff70ca5b3434a37b800e473d
João <joao@joao.codes>
2021-12-27 17:28:32 +0000

    tutorial rotating box

---
+++ src/routes/index.svelte
@@ -0,0 +1,44 @@
+<script>
+	import * as THREE from 'three';
+	import * as SC from 'svelte-cubed';
+
+	let spin = 0;
+
+	SC.onFrame(() => {
+		spin += 0.01;
+	});
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<SC.Canvas
+	antialias
+	background={new THREE.Color('rgb(20, 10, 20)')}
+	fog={new THREE.FogExp2('#fff', 0.1)}
+	shadows
+>
+	<SC.Mesh
+		geometry={new THREE.BoxGeometry()}
+		material={new THREE.MeshStandardMaterial({ color: 0xffffff })}
+		rotation={[0, spin, 0]}
+		castShadow
+	/>
+	<SC.PerspectiveCamera position={[2, 1, 2]} />
+	<SC.OrbitControls enableZoom maxPolarAngle={Math.PI * 0.51} />
+	<SC.AmbientLight intensity={0.7} />
+	<SC.DirectionalLight intensity={0.2} position={[-5, 10, 2]} shadow={{ mapSize: [2048, 2048] }} />
+	<SC.Group position={[0, -1 / 2, 0]}>
+		<SC.Mesh
+			geometry={new THREE.PlaneGeometry(50, 50)}
+			material={new THREE.MeshStandardMaterial({ color: '#d4d4d4' })}
+			rotation={[-Math.PI / 2, 0, 0]}
+			receiveShadow
+		/>
+
+		<SC.Primitive
+			object={new THREE.GridHelper(50, 50, 0x444444, 0x555555)}
+			position={[0, 0.001, 0]}
+		/>
+	</SC.Group>
+</SC.Canvas>
